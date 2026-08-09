b772af92a2bf2f3d64cc799e1e7aa6443b38deff
exusr <pietro.nuzzo@protonmail.com>
2026-08-09 20:19:37 +0200

    chore: remove unused debug_script utility file

--- lib/widgets/editor/debug_script.dart
+++
@@ -1,5 +0,0 @@
-import 'package:fluent_editor/fluent_document.dart';
-void debugNode(FluentDocument doc, String id) {
-  final node = doc.nodeById(id);
-  print("Found node: ${node?.runtimeType}");
-}
